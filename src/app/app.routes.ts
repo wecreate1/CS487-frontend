95252778a17b3dcac19800538832e58dcbee1b5d
wecreate1 <jimmy@sallenback.net>
2024-12-02 23:18:46 -0600

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StartStudyComponent } from './start-study/start-study.component';
+import { StudyComponent } from './study/study.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'dashboard', component: DashboardComponent},
+    { path: 'startStudy', component: StartStudyComponent},
+    { path: 'study/:id', component: StudyComponent},
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+];
